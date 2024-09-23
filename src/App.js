180fa1e22f59ef4a80db70a36e1e3a6ac5cbe38d
thakurshardul <thakurshardul1@gmail.com>
2024-09-24 04:12:30 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import About from "./About";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <About />
+    </div>
+  );
+}
